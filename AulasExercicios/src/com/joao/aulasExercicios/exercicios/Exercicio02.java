9c046c0434284881e736d407125102d3ba4f795e
joaumsud <joaumsud@hotmail.com>
2021-10-01 11:52:48 -0300

    Fazendo os primeiros exercicios (01 e 02)

--- AulasExercicios/src/com/joao/aulasExercicios/exercicios/Exercicio02.java
+++ AulasExercicios/src/com/joao/aulasExercicios/exercicios/Exercicio02.java
@@ -1,10 +1,16 @@
 package com.joao.aulasExercicios.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Digite um número qualquer");
+		
+		Scanner scan = new Scanner(System.in);
+		int numero = scan.nextInt();
 
+		System.out.println("O número digitado é: " + numero);
 	}
 
 }
