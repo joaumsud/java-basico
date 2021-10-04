1d4fe6e7a9dfa38728e635fbdf109ce1cbbb0941
joaumsud <joaumsud@hotmail.com>
2021-10-03 21:36:34 -0300

    Exercicio area do circulo

--- AulasExercicios/src/com/joao/aulasExercicios/exercicios/Exercicio04.java
+++ AulasExercicios/src/com/joao/aulasExercicios/exercicios/Exercicio04.java
@@ -1,10 +1,14 @@
 package com.joao.aulasExercicios.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
-
+	// 4. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite o valor do Raio: ");
+		double raio = scan.nextDouble();
+		System.out.println("A área do circulo é: " + ((raio * raio) * 3.1415));
 	}
 
 }
